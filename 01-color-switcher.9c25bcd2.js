a213a10b6ab1c57d8bf8f42beff2e2027686832f
UmyaFamilia <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-29 12:52:52 +0000

    Deploying to gh-pages from @ UmyaFamilia/goit-js-hw-09@c22c70f1818a676ae40d4c19476b62756517b816 🚀

---
+++ 01-color-switcher.9c25bcd2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=0;t.addEventListener("click",(()=>{a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","true")})),e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.9c25bcd2.js.map
