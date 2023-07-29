b90b35600fe0702d31156f4046c470102982681e
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-29 15:55:01 +0300

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,66 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+const delay = document.querySelector(`[name="delay"]`);
+const step = document.querySelector(`[name="step"]`);
+const amount = document.querySelector(`[name="amount"]`);
+//
+//
+//
+//
+//
+form.addEventListener('submit', start);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+function start(evnt) {
+  evnt.preventDefault();
+  let delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayValue += stepValue;
   }
 }
+//
+//
+//
+//
+//
+//
+//
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+
+        resolve({ position, delay });
+      } else {
+        // Reject
+
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+//
+//
+//
+//
+//
