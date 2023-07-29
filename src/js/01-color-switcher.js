c22c70f1818a676ae40d4c19476b62756517b816
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-29 15:52:31 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const start = document.querySelector('[data-start]');
+const end = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let startId = 0;
+start.addEventListener('click', () => {
+  startId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  start.setAttribute('disabled', 'true');
+});
 
+end.addEventListener('click', () => {
+  clearInterval(startId);
+  start.removeAttribute('disabled');
+});
